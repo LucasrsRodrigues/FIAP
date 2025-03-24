d25ca289dd2014095758328daaf01439f27c62ee
lucasrsrodrigues <lucasrs.rodrigues3@outlook.com>
2025-03-24 10:14:20 -0300

    add: mock-bank code

---
+++ Projetos/mock-bank/app/index.tsx
@@ -0,0 +1,24 @@
+import { View } from "@/components/Themed";
+import { StyleSheet, Text } from "react-native";
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Hello world!
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
